80bca285f25a66def5ad5ee3602a2f94007b9260
Arion <arionnagappen@gmail.com>
2025-05-22 16:37:42 +0200

    added comments to developer policy

--- terraform/security/iam_group_policies.tf
+++ terraform/security/iam_group_policies.tf
@@ -6,12 +6,15 @@ resource "aws_iam_policy" "developer_policy" {
   policy = jsonencode({
     "Version": "2012-10-17",
     "Statement": [
+
+      # EC2 Managment
       {
         "Effect": "Allow",
         "Action": "ec2:*",
         "Resource": "*"
       },
 
+      # Read & Write to S3
       {
         "Effect": "Allow",
 			  "Action": [
@@ -22,6 +25,7 @@ resource "aws_iam_policy" "developer_policy" {
 			  "Resource": "arn:aws:s3:::s3-frontend-bucket-an"
       },
 
+      # View CloudWatch Logs
       {
         "Effect": "Allow",
         "Action": [
@@ -34,7 +38,6 @@ resource "aws_iam_policy" "developer_policy" {
       }
     ]
   })
-
 }
 
 // --- OPERATIONS POLICY ---
