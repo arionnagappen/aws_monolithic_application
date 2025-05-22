c8f4cad372c8d73b83a3d495f0fa56c92cfbbd14
Arion <arionnagappen@gmail.com>
2025-05-22 17:40:02 +0200

    Operations Policy

--- terraform/security/iam_groups.tf
+++ terraform/security/iam_groups.tf
@@ -7,3 +7,13 @@ resource "aws_iam_group_policy_attachment" "developers_policy_attach" {
   group = aws_iam_group.developers_group.name
   policy_arn = aws_iam_policy.developer_policy.arn
 }
+
+// --- OPERATIONS ---
+resource "aws_iam_group" "operations_group" {
+  name = "OperationsSecurityGroup"
+}
+
+resource "aws_iam_group_policy_attachment" "operations_policy_attach" {
+  group = aws_iam_group.operations_group.name
+  policy_arn = aws_iam_policy.operations_policy.arn
+}
