ba0235d40f12506e63f8735e8ef2c61622db86f7
Arion <arionnagappen@gmail.com>
2025-05-30 22:39:09 +0200

    added encryption to user data bucket

--- terraform/storage/s3_user_data.tf
+++ terraform/storage/s3_user_data.tf
@@ -1,5 +1,16 @@
 resource "aws_s3_bucket" "user_data_bucket" {
   bucket = "s3-user-data-bucket"
+
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "user_data_encryption" {
+  bucket = aws_s3_bucket.user_data_bucket.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
 }
 
 resource "aws_s3_bucket_public_access_block" "user_data_block" {
