88e02a6c785e3f5f1b9fbb1106e182fa172fe85b
Arion <arionnagappen@gmail.com>
2025-07-11 20:16:56 +0200

    Refactored network module

--- terraform/modules/network/variables.tf
+++ terraform/modules/network/variables.tf
@@ -1,4 +1,6 @@
-// NETWORKING VARIABLES
+// NETWORKING VARIABLES //
+
+// Vpc Variables//
 variable "vpc_cidr" {
   type = string
   description = "VPC CIDR Block"
@@ -7,9 +9,21 @@ variable "availability_zones" {
   type = list(string)
   description = "List of Availability Zones"
 }
+
+variable "vpc_tag" {
+  type = string
+  description = "Tag the VPC with a name"
+}
+
+// Subnet Variables //
 variable "public_subnet_cidrs" {
   type = list(string)
   description = "Public Subnet CIDR values"
+}
+
+variable "pub_subnet_tag" {
+  type = string
+  description = "Tag the Public Subnet with a name"
 }
 
 variable "app_subnet_cidrs" {
@@ -17,7 +31,17 @@ variable "app_subnet_cidrs" {
   description = "Private Subnet CIDR values"
 }
 
+variable "app_subnet_tag" {
+  type = string
+  description = "Tag the App Subnet with a name"
+}
+
 variable "database_subnet_cidrs" {
   type = list(string)
   description = "Database Subnet CIDR values"
+}
+
+variable "database_subnet_tag" {
+  type = string
+  description = "Tag the Database Subnet with a name"
 }
