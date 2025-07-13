25d89bacf78f6c3718f1d2fecac183754d40128c
Arion <arionnagappen@gmail.com>
2025-07-13 17:02:04 +0200

    added compute changes to network branch

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-// --- NETWORKING ---
+// --- network ---
 module "network" {
   source = "./modules/network"
 
@@ -12,31 +12,56 @@ module "network" {
   pub_subnet_tag = "PublicSubnet"
   app_subnet_tag = "AppSubnet"
   database_subnet_tag = "DatabaseSubnet"
-
-  
 }
 
 // --- COMPUTE ---
 module "compute" {
   source = "./modules/compute"
 
+  // Launch Template //
+  name_prefix = "App Server-"
   inst_ami  = "ami-0914bddde8faa93a0"
   inst_type = "t3.micro"
+  launch_temp_tag = "MyLaunchTemp"
 
-  // Networking IDs
+  // Auto Scaling Group //
+  asg_name = "App Server ASG"
+  maximum_size = 3
+  minimum_size = 1
+  desired_cap = 2
+  asg_tag = "AppServer"
+  health_chk_type = "ELB"
+  health_chk_grace_period = 300
+
+  // Application Load Balancer //
+  alb_tg_name = "ApplicationTargetGroup"
+  alb_protocol = "HTTP"
+  alb_health_interval = 30
+  alb_health_timeout = 5
+  alb_healthy_threshold = 5
+  alb_unhealthy_threshold = 2
+  alb_health_matcher = 200
+  alb_name = "ApplicationLoadBalancer"
+
+  // Bastion Host //
+  bastion_inst_ami = "ami-0914bddde8faa93a0"
+  bastion_inst_type = "t3.micro"
+  bastion_tag = "BastionHost"
+
+  // Import network IDs
   vpc_id              = module.network.vpc_id
   availability_zones  = module.network.availability_zones
   public_subnet_ids   = module.network.public_subnet_ids
   app_subnet_ids      = module.network.app_subnet_ids
   database_subnet_ids = module.network.database_subnet_ids
 
-  // App Server Instance Profile Name
+  // Import App Server Instance Profile Name
   app_server_instance_profile_name = module.security.app_server_instance_profile_name
 }
 
 // --- DATABASE ---
 module "database" {
-  source = "./database"
+  source = "./modules/database"
 
   db_identifier   = "my-app-db"
   db_engine       = "mysql"
@@ -45,11 +70,11 @@ module "database" {
   db_storage_size = 10
   secret_name     = "rds-credentials"
 
-  // Networking IDs
-  vpc_id              = module.networking.vpc_id
-  availability_zones  = module.networking.availability_zones
-  app_subnet_ids      = module.networking.app_subnet_ids
-  database_subnet_ids = module.networking.database_subnet_ids
+  // network IDs
+  vpc_id              = module.network.vpc_id
+  availability_zones  = module.network.availability_zones
+  app_subnet_ids      = module.network.app_subnet_ids
+  database_subnet_ids = module.network.database_subnet_ids
 
   // Compute IDs
   app_server_sg_id = module.compute.app_server_sg_id
@@ -60,19 +85,19 @@ module "database" {
 
 // --- FRONTEND --- 
 module "frontend" {
-  source = "./frontend"
+  source = "./modules/frontend"
 }
 
 // --- STORAGE ---
 module "storage" {
-  source = "./storage"
+  source = "./modules/storage"
 
   kms_key_arn = module.security.kms_key_arn
 }
 
 // --- SECURITY ---
 module "security" {
-  source = "./security"
+  source = "./modules/security"
 
   user_data_arn = module.storage.user_data_arn
 }
