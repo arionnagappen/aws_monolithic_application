d09d363ff4336df20a0c92940ca3a6d5099f277f
Arion <arionnagappen@gmail.com>
2025-07-13 16:58:55 +0200

    Fixed tag variables in main.tf and changed networking to network

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,27 +1,34 @@
 // --- NETWORKING ---
 module "networking" {
-  source = "./networking"
+  source = "./modules/network"
 
   vpc_cidr              = "10.0.0.0/16"
   availability_zones    = ["af-south-1a", "af-south-1b"]
   public_subnet_cidrs   = ["10.0.1.0/24", "10.0.2.0/24"]
   app_subnet_cidrs      = ["10.0.3.0/24", "10.0.4.0/24"]
   database_subnet_cidrs = ["10.0.5.0/24", "10.0.6.0/24"]
+
+  vpc_tag = "MyVPC"
+  pub_subnet_tag = "PublicSubnet"
+  app_subnet_tag = "AppSubnet"
+  database_subnet_tag = "DatabaseSubnet"
+
+  
 }
 
 // --- COMPUTE ---
 module "compute" {
-  source = "./compute"
+  source = "./modules/compute"
 
   inst_ami  = "ami-0914bddde8faa93a0"
   inst_type = "t3.micro"
 
   // Networking IDs
-  vpc_id              = module.networking.vpc_id
-  availability_zones  = module.networking.availability_zones
-  public_subnet_ids   = module.networking.public_subnet_ids
-  app_subnet_ids      = module.networking.app_subnet_ids
-  database_subnet_ids = module.networking.database_subnet_ids
+  vpc_id              = module.network.vpc_id
+  availability_zones  = module.network.availability_zones
+  public_subnet_ids   = module.network.public_subnet_ids
+  app_subnet_ids      = module.network.app_subnet_ids
+  database_subnet_ids = module.network.database_subnet_ids
 
   // App Server Instance Profile Name
   app_server_instance_profile_name = module.security.app_server_instance_profile_name
